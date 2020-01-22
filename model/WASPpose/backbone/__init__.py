e0fef68e344e649cf81921699f8672ba2dfd7154
Bruno Artacho <44781803+bmartacho@users.noreply.github.com>
2020-01-22 14:47:42 -0500

    Add files via upload

--- model/WASPpose/backbone/__init__.py
+++ model/WASPpose/backbone/__init__.py
@@ -1,1 +1,7 @@
+from model.WASPpose.backbone import resnet
 
+def build_backbone(backbone, output_stride, BatchNorm):
+    if backbone == 'resnet':
+        return resnet.ResNet101(output_stride, BatchNorm)
+    else:
+        raise NotImplementedError
